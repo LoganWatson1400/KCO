300b428ff4102247b66ea3db4b531bc6f3b5ddda
HawkerX <jackharmer5@gmail.com>
2024-10-25 13:49:49 -0500

    fix: removed magic numbers from Train.py
    made BatchSize and epochs constants in NN.
    added general outline of what train should do.
    added placeholders for unassigned values

--- HackathonPackageV1/Train.py
+++ HackathonPackageV1/Train.py
@@ -1,10 +1,14 @@
+# Training Program for NN model
 import NN as n
 
 
-batch_size = 128
-epochs = 15
+## Grab Data
 
+## Load data
+x_train = None #PlaceHolder
+y_train = None #PlaceHolder
 
+## Training Loop (3 epochs = 3 weeks)
 n.model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-n.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+n.model.fit(x_train, y_train, batch_size=n.BATCH_SIZE, epochs=n.EPOCHS, validation_split=0.1)
