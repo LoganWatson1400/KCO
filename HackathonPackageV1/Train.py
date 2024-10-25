250ffb66ca8fd5e577ba00f4f5563f530b256f37
HawkerX <jackharmer5@gmail.com>
2024-10-25 15:36:51 -0500

    feat: added autoscore loss function for training
    This is a modified autoscore that only returns totalscore. Effectively....
    loss score = autoscore

--- HackathonPackageV1/Train.py
+++ HackathonPackageV1/Train.py
@@ -1,6 +1,6 @@
 # Training Program for NN model
 import NN as n
-
+import Loss_Function as lf
 
 ## Grab Data
 
@@ -8,7 +8,17 @@ import NN as n
 x_train = None #PlaceHolder
 y_train = None #PlaceHolder
 
+date = [
+    '2024-09-06 Week 1',
+    '2024-09-06 Week 2',
+    '2024-09-06 Week 3'
+    ]
+
+situationRoot='HackathonPackageV1\DataCache\OptimizerSituations'
 ## Training Loop (3 epochs = 3 weeks)
-n.model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+autoscore = lf.autoscore_loss(situationRoot, date[0]) #Loss Function
+
+n.model.compile(loss=autoscore, optimizer="adam", metrics=["accuracy"])
 
 n.model.fit(x_train, y_train, batch_size=n.BATCH_SIZE, epochs=n.EPOCHS, validation_split=0.1)
