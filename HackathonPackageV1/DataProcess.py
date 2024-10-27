736ed564a42a9d7ec38de140ff42c7a4d0695cd6
LoganWatson1400 <lawatson@uwm.edu>
2024-10-27 15:04:47 -0500

    fix: modified updatejson to update all values in json while maintaining fromatting

--- HackathonPackageV1/DataProcess.py
+++ HackathonPackageV1/DataProcess.py
@@ -21,43 +21,43 @@ situation = ['planningSchedule',
     'scrapFactor',
     'currentTimeUTC']
 
+# extractPath = f'HackathonPackageV1/DataCache/OptimizerSituations/{d}/{s}.json'
+# exportPath = f'HackathonPackageV1/EX_DataCache/OptimizerSituations/{d}/{s}.json'
+
+
 def __getData(d):
     data = {}
     for s in situation:
         extractPath = f'HackathonPackageV1/EX_DataCache/OptimizerSituations/{d}/{s}.json'
-        file = __getDataSingle(d, extractPath)
+        file = __getDataSingle(extractPath)
         data.update(file)
     return data
 
-def __getDataSingle(d, extractPath):
+def __getDataSingle(extractPath):
     data = {}
     for file in glob.glob(extractPath):
         with open(file) as f:
             data.update(json.load(f))
     return data
 
-def updateJson(predictions):
-    for i in enumerate(predictions):
-        pass
-
-
 def __normalize(data):
     return pd.json_normalize(data)
 
 def __clean_data(df):
+
     # Identify numeric columns
     numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
     
     # Encode numeric columns
     encoded_numeric = pd.get_dummies(df[numeric_cols]).reset_index(drop=True)
-    
+  
     # Identify categorical columns
     categorical_cols = df.select_dtypes(include=['object']).columns
     
     # Encode categorical data
     encoder = OneHotEncoder(sparse=False)
     encoded_categorical = encoder.fit_transform(df[categorical_cols])
-    
+
     # Create encoded columns
     encoded_categorical_df = pd.DataFrame(encoded_categorical, 
                                        columns=[f'{col}_{val}' for col in categorical_cols for val in encoder.categories_[list(categorical_cols).index(col)]])
@@ -65,7 +65,7 @@ def __clean_data(df):
     
     # Combine encoded data
     final_df = pd.concat([encoded_numeric, encoded_categorical_df], axis=1)
-    
+
     return final_df
 
 def toCV(data):
@@ -75,14 +75,45 @@ def toCV(data):
 def processData(d):
     data = __getData(d)
     df = __normalize(data)
+    # cleaned_df = df ## TODO bypasses cleaning function for convinience
     cleaned_df = __clean_data(df)
     return cleaned_df
 
 def updatejson(d, preds):
     predNumber = 0
     for s in situation:
-        extractPath = f'HackathonPackageV1/EX_DataCache/OptimizerSituations/{d}/{s}.json'
-        prevData = __getDataSingle(d,extractPath)
-        for x in preds:
-            for y in x:
-                pass
+        extractPath = f'HackathonPackageV1/DataCache/OptimizerSituations/{d}/{s}.json'
+        exportPath = f'HackathonPackageV1/EX_DataCache/OptimizerSituations/{d}/{s}.json'
+        prevData = __getDataSingle(extractPath)
+        
+        #iterator for preds
+        pVals = (item[0] for item in preds)
+
+        
+        for key in prevData:
+            try:
+                temp = next(pVals)
+                # print(temp,end=" : ")
+                # if temp == None or temp == 'nan':
+                #     prevData[key] = 0
+                # else:
+                prevData[key] = next(pVals)
+            except StopIteration:
+                print('',end='')
+            # prevData[key] = prevData[key]
+
+        with open(exportPath,'w') as json_file:
+            json.dump(prevData, json_file, indent=4, separators= (',', ':'))
+
+
+
+# data = {}
+# for s in situation:
+#     extractPath = f'HackathonPackageV1/EX_DataCache/OptimizerSituations/{d}/{s}.json'
+#     for file in glob.glob(extractPath):
+#         with open(file) as f:
+#             temp = json.load(f)
+#             print(temp)
+#             data.update(json.load(f))
+#     data.update(file)
+
