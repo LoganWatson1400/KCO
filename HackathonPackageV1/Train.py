5dd80f08cc92eee738fd36d491b8f7b9b28772b4
HawkerX <jackharmer5@gmail.com>
2024-10-27 20:15:37 -0500

    feat: sanboc bs that somehow made NN work :)

--- HackathonPackageV1/Train.py
+++ HackathonPackageV1/Train.py
@@ -5,7 +5,7 @@ import DataProcess as dp
 import numpy as np
 
 BATCH_SIZE = 1
-EPOCHS = 1
+EPOCHS = 10
 
 date = [
     '2024-09-06 Week 1',
@@ -22,7 +22,7 @@ dp.toCV(data)
 x_train = np.array(data, dtype=np.float32).reshape(BATCH_SIZE,len(data.columns))
 y_train = x_train.astype(np.float32)
 situationRoot='HackathonPackageV1\DataCache\OptimizerSituations'
-
+predictRoot='HackathonPackageV1\PredDataCache\OptimizerSituations'
 ####MODEL####
 
 # Initialize Input Layer
@@ -30,12 +30,12 @@ model = k.Sequential()
 model.add(k.Input(shape=INPUT_SIZE))
 
 # Initialize Hidden Layer
-model.add(k.layers.Dense(INPUT_SIZE/2, activation="relu"))
-model.add(k.layers.Dense(INPUT_SIZE/2, activation="relu"))
-model.add(k.layers.Dense(INPUT_SIZE/2, activation="relu"))
+model.add(k.layers.Dense(INPUT_SIZE/2, activation="tanh"))
+model.add(k.layers.Dense(INPUT_SIZE/2, activation="tanh"))
+model.add(k.layers.Dense(INPUT_SIZE/2, activation="tanh"))
 
 # Initialize Output Layer
-model.add(k.layers.Dense(INPUT_SIZE, activation="relu"))
+model.add(k.layers.Dense(INPUT_SIZE, activation="tanh"))
 
 # OPTIONAL model check
 model.summary()
@@ -44,17 +44,28 @@ model.summary()
 
 ## Training Loop (3 epochs = 3 weeks)
 
-autoscore = lf.autoscore_loss(situationRoot, date[0]) #Loss Function
+autoscore = lf.autoscore_loss(predictRoot, date[0]) #Loss Function
 
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.compile(loss=autoscore, optimizer="adam", metrics=["accuracy"])
 
 # model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS)
 for i in range(EPOCHS):
-    model.train_on_batch(x_train, y_train)
+    epoch_data = model.train_on_batch(x_train, y_train, return_dict= True)
     predictions = model.predict_on_batch(x_train)
-    # print(len(predictions))
-    for x in predictions:
-        for y in x:
-            print(f'{y}')
+    print(f"\nEpoch: {i+1} --- Loss: {epoch_data['loss']} --- Accuracy: {epoch_data['accuracy']} --- prediction[0][0]: {predictions[0][0]}\n")
+    # print(len(predictions[0]))
+    # print("\n\n")
+    # print(epoch_data)
+    # print("\n\n")
     # print(predictions[0][0])
+    # print("\n\n")
+    dp.updatejson(date[0],predictions)
+    weights = model.get_weights()
+    model.compile(loss=autoscore, optimizer="adam", metrics=["accuracy"])
+    model.set_weights(weights)
+#     # # print(len(predictions))
+#     # for x in predictions:
+#     #     for y in x:
+#     #         print(f'{y}')
+#     # # print(predictions[0][0])
 
