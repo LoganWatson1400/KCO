27ca29a54012c3921244e722ded8dbbcc96f0edb
LoganWatson1400 <lawatson@uwm.edu>
2024-10-29 20:22:19 -0500

    fix: new default

--- HackathonPackageV1/Train.py
+++ HackathonPackageV1/Train.py
@@ -14,43 +14,42 @@ weeks = [
 ]
 
 #Static paths
-staticPath = f'HackathonPackageV1\\DataCache\\OptimizerSituations\\ {weeks[week]}\\planningSchedule.json'
+staticPath = f'HackathonPackageV1\\DataCache\\OptimizerSituations\\{weeks[week]}\\planningSchedule.json'
 root = 'HackathonPackageV1\\DataCache\\OptimizerSituations'
-StaticPaths = glob.glob(root + f'\\{weeks[week]}\\*.json')
+InitialPaths = glob.glob(root + f'\\{weeks[week]}\\*.json')
+temp = {}
+for i in InitialPaths:
+    temp[os.path.basename(i)] = i
+InitialPaths = temp
+
+# print(InitialPaths['initialPOs.json']) #How to get
 
 #Output Paths
 outRoot = 'HackathonPackageV1\\PredDataCache\\OptimizerSituations'
-outPath = f'HackathonPackageV1\\PredDataCache\\OptimizerSituations\\{weeks[week]}\\planningSchedule.json'
+outSchedule = f'HackathonPackageV1\\PredDataCache\\OptimizerSituations\\{weeks[week]}\\planningSchedule.json'
 
 #TempPaths
 bestSchedule = 'HackathonPackageV1\\BestSchedule.json'
 
-df = pd.read_json('HackathonPackageV1\\DataCache\\OptimizerSituations\\2024-09-06 Week 1\\planningSchedule.json')
+df = pd.read_json(staticPath)
+IData = pd.read_json(InitialPaths['initialPOs.json'])
+
 ### Catagorical Data ###
-PUnits = df['ProductionUnit'].unique()
-PIds = df['Prod_Id'].unique()
+PUnits = IData['ProductionUnit'].unique()
+PIds = IData['Prod_Id'].unique()
 
 ### Float Values ###
-ForeStartMIN = df['ForecastStartTime'].min()
-ForeStartMAX = df['ForecastStartTime'].max()
+ForeStartMIN = IData['ForecastStartTime'].min()
+ForeStartMAX = IData['ForecastStartTime'].max()
 
-ForeEndMIN = df['ForecastEndTime'].min()
-ForeEndMAX = df['ForecastEndTime'].max()
+ForeEndMIN = IData['ForecastEndTime'].min()
+ForeEndMAX = IData['ForecastEndTime'].max()
  
 
 
 
 
 ### randomizer ###TODO
-
-### Randomization function
-def randomize_value(df, column):
-    if column in ['ProductionUnit', 'Prod_Id']:
-        return np.random.choice(df[column].unique())
-    elif column == 'ForecastStartTime':
-        return np.random.uniform(ForeStartMIN, ForeStartMAX)
-    elif column == 'ForecastEndTime':
-        return np.random.uniform(ForeEndMIN, ForeEndMAX)
 
 # Disable
 def blockPrint():
@@ -61,7 +60,7 @@ def enablePrint():
     sys.stdout = sys.__stdout__
 
 
-(best, z) = officialScorer(root, weeks[week])
+best = -364453
 iterations = 0
 patients = 0
 # blockPrint() ##########################################################
@@ -70,15 +69,15 @@ while iterations < 4 and patients < 5:
     patients += 1
     
 
-    # new_PUnits = random.choices(PUnits, k=len(df['ProductionUnit']))
-    # new_PIds = random.choices(PIds, k=len(df['Prod_Id']))
+    new_PUnits = random.choices(PUnits, k=len(df['ProductionUnit']))
+    new_PIds = random.choices(PIds, k=len(df['Prod_Id']))
 
-    # df['ProductionUnit'] = new_PUnits
-    # df['Prod_Id'] = new_PIds
+    df['ProductionUnit'] = new_PUnits
+    df['Prod_Id'] = new_PIds
 
     #TODO random start and end time
 
-    # df.to_json(outPath, indent=4)
+    df.to_json(outSchedule, indent=4)
     (loss, z) = officialScorer(outRoot, weeks[week]) #week is default #TODO can use breakdown to optimize
     # print(f'loss: {loss}: breakdown: {breakdown}')
     if loss > best:
@@ -92,7 +91,7 @@ while iterations < 4 and patients < 5:
 
 print(f'Best Score achived: {best}')
 temp = pd.read_json(bestSchedule)
-temp.to_json(outPath, indent=4)
+temp.to_json(outSchedule, indent=4)
 officialScorer(outRoot, weeks[week])
 
 
